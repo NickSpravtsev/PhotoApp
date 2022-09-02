71a61006624dca3b62829abdca2954b6525ea76c
Nikolay Spravtsev <nicksprv@icloud.com>
2022-09-03 01:38:57 +0300

    AddBarButton for album navigation bar added

--- PhotoApp/Sources/AlbumPhotoViewController.swift
+++ PhotoApp/Sources/AlbumPhotoViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class AlbumPhotoViewController: UIViewController {
 
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        setupNavigationBar()
+        setupHierarchy()
+        setupLayout()
+    }
+
+    // MARK: Setup
+
+    private func setupNavigationBar() {
         title = "Альбомы"
         navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addBarButtonPressed))
+    }
+
+    private func setupHierarchy() {
+
+    }
+
+    private func setupLayout() {
+
+    }
+
+    // MARK: - Actions
+
+    @objc private func addBarButtonPressed() {
+        print("Add bar button pressed")
     }
 }
 
