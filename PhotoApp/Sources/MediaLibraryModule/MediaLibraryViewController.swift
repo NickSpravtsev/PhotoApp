cdb16fe49fc40a017b689c8a87c33444b830fabc
Nikolay Spravtsev <nicksprv@icloud.com>
2022-09-08 02:13:14 +0300

    Add content into media library

--- PhotoApp/Sources/MediaLibraryModule/MediaLibraryViewController.swift
+++ PhotoApp/Sources/MediaLibraryModule/MediaLibraryViewController.swift
@@ -9,37 +9,42 @@ import UIKit
 import SnapKit
 
 class MediaLibraryViewController: UIViewController {
-
+    
+    // MARK: - Propierties
+    
+    private var mediaLibraryItems: [AlbumItem]?
+    
     // MARK: - Outlets
-
+    
     private lazy var mediaCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-
+        
         collectionView.register(MediaLibraryCell.self, forCellWithReuseIdentifier: MediaLibraryCell.identifier)
-
+        
         collectionView.dataSource = self
         collectionView.delegate = self
-
+        
         return collectionView
     }()
-
+    
     // MARK: - Lifecycle
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Медиатека"
         navigationController?.navigationBar.prefersLargeTitles = true
+        mediaLibraryItems = AlbumItem.mediaLibraryItems
         setupHierarchy()
         setupLayout()
     }
-
+    
     // MARK: - Setup
-
+    
     private func setupHierarchy() {
         view.addSubview(mediaCollectionView)
     }
-
+    
     private func setupLayout() {
         mediaCollectionView.snp.makeConstraints { make in
             make.top.right.bottom.left.equalTo(view)
@@ -51,19 +56,20 @@ class MediaLibraryViewController: UIViewController {
 
 extension MediaLibraryViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return mediaLibraryItems?.count ?? 0
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = mediaCollectionView.dequeueReusableCell(withReuseIdentifier: MediaLibraryCell.identifier, for: indexPath)
-        return cell
+        let cell = mediaCollectionView.dequeueReusableCell(withReuseIdentifier: MediaLibraryCell.identifier, for: indexPath) as? MediaLibraryCell
+        cell?.cellItem = mediaLibraryItems?[indexPath.row]
+        return cell ?? UICollectionViewCell()
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let size = CGSize(width: view.frame.size.width - 40, height: view.frame.size.height / 3.5)
         return size
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 25
     }
